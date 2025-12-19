105fd74b6c5c5708938413a441e61308768e4eb7
jin-park0115 <jinpark0115@naver.com>
2025-12-19 14:13:38 +0900

    메모리 Map말고, h2 db연동

--- src/main/java/com/example/allstroebe/MemberController.java
+++ src/main/java/com/example/allstroebe/MemberController.java
@@ -17,30 +17,38 @@ public class MemberController {
     // 이제 Map 대신 Repository 사용.
     // key: 아이디, value: 비밀번호
     private  static  Map<String, String> db = new HashMap<>();
+    private final MemberRepository memberRepository;
 
     //1. 회원가입 API
     @PostMapping("/join") // 중요한 정보는 POST로 보낸다.
     public String join(@RequestBody MemberDto memberDto) {
         //@RequestBody: 보내온 JSON 데이터를 MemberDto 객체로 넣어 달라는 뜻
-        if(db.containsKey(memberDto.getEmail())){
-            return "이미 존재하는 아이디입니다.";
+        // 중복 체크 (DB에서 조회)
+
+        if(memberRepository.findByemail(memberDto.getEmail()).isPresent()){
+            return "이미 존재하는 이메일 입니다.";
         }
-        db.put(memberDto.getEmail(), memberDto.getPassword());
-        return "회원가입 성공!";
+        // Entity 객체 생성 후 DB에 저장.
+        Member member = new Member();
+        member.setEmail(memberDto.getEmail());
+        member.setPassword(memberDto.getPassword());
+
+        memberRepository.save(member); //DB에 저장
+        return "회원가입 성공! DB에 저장 되었습니다";
     }
 
     //2. 로그인 API
     @PostMapping("/login")
     public String login(@RequestBody MemberDto memberDto) {
-        String savedPassword = db.get(memberDto.getEmail());
-
-        if(savedPassword == null){
-            return "없는 아이디입니다";
-        }
-        if(!savedPassword.equals(memberDto.getPassword())){
-            return "비밀번호가 틀렸습니다.";
-        }
-        return "로그인 성공! 환영합니다";
+        // DB에서 아이디로 찾기
+      return memberRepository.findByemail(memberDto.getEmail())
+              .map(m ->{
+                  if (m.getPassword().equals(memberDto.getPassword())){
+                      return "로그인 성공! 환영합니다";
+                  }
+                  return "비밀번호가 틀렸습니다.";
+              })
+              .orElse("존재하지 않는 사용자입니다.");
     }
 
 }
